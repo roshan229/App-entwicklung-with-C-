58f7ec49629aabb6215134ee21b2242f1f57d858
roshan229 <rosanshiwakoti321@gmail.com>
2025-01-14 17:13:15 +0100

    app develp setup

---
+++ MycSharpApp/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Framwork opensource 
+    applikaiton entwicklung
+*/
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*int zahl = 42;                                        // ganze Zahl
+        double pi = 3.14159;                                  // Gleitkommazahl
+        string nachricht = "Hallo, Welt!";                    // Zeichenkette
+        bool istWahr = true;                                  // boolesch
+
+        Console.WriteLine($"Zahl: {zahl}");
+        Console.WriteLine($"Pi: {pi}");
+        Console.WriteLine($"Nachricht: {nachricht}");
+        Console.WriteLine($"Ist Wahr: {istWahr}");*/
+
+        Console.WriteLine("Hello World");
+
+        
+    }
+}
