ed04bd77db7171c061bb6a2edfc417f0fa5b5262
roshan229 <rosanshiwakoti321@gmail.com>
2025-01-14 18:00:38 +0100

    completed basic course of C#

--- MycSharpApp/Program.cs
+++ MycSharpApp/Program.cs
@@ -7,7 +7,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*int zahl = 42;                                        // ganze Zahl
+
+        static void Greet( ) {
+            Console.WriteLine("Good morning");
+        }
+        int zahl = 42;
+        /*                                        // ganze Zahl
         double pi = 3.14159;                                  // Gleitkommazahl
         string nachricht = "Hallo, Welt!";                    // Zeichenkette
         bool istWahr = true;                                  // boolesch
@@ -18,7 +23,29 @@ class Program
         Console.WriteLine($"Ist Wahr: {istWahr}");*/
 
         Console.WriteLine("Hello World");
+        Console.WriteLine("Zahl: "+zahl +"\n");
 
-        
+  
+    /*string imp = Console.ReadLine(); //takes the inpute
+    Console.WriteLine(imp);*/
+
+       float a=34.4F;  //F lekhna imp xa else double use garnu parxa
+
+       //string to int conversion
+
+       string num="2";
+       Console.WriteLine("Numer with additoin  5  is "+ (Convert.ToInt32(num)+5));
+
+       /*
+        /" for anführungszeichen
+       */
+
+       Greet();
+
+    Player tommy =new Player();
+    Console.WriteLine(tommy.health);
+    tommy.health=1000;  //to set the new health
+    Console.WriteLine(tommy.health);
+
     }
 }
---
+++ MycSharpApp/player.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+class Player {
+
+    public string name="Roshan";
+    public int health =234;
+
+    
+
+}
